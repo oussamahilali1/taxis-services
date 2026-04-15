c7316f26c7a85af9da824186ebda23a9d281e571
YounsseAmazzal <amazzalyounsse@gmail.com>
2026-04-15 10:29:34 +0100

    front

--- backend/models/Demandeapide.go
+++
@@ -1,6 +0,0 @@
-package models
-type Demandeapide struct {
-	ID           int    `json:"id"` 
-	Adressededépart   string `json:"adresse_de_départ"`
-	Contact        string `json:"contact"`
-}
--- backend/models/errors.go
+++
@@ -1,5 +0,0 @@
-package models
-type ErrorData struct {
-	Code    int
-	Message string
-}
